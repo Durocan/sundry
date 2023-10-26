9541f5a73853147e954a9885656225d7c10ea791
Deckard Kwon <prodigy9090@gmail.com>
2023-10-26 10:18:31 +0900

    branch test for merge

--- acquire/20231025_testing_branch/sr01.dart
+++ acquire/20231025_testing_branch/sr01.dart
@@ -1,3 +1,13 @@
 void main() {
   print("everyday");
+
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+  
+  List<int> numbers = [20, 68, 89, 251];
+  
+  int sum = numbers.fold(9, (e, p) => e + p);
+  print(sum);
+
 }
