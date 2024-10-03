6537f95d14df870ebe34b1cdccfbfb82e83817aa
Deckard <prodigy9090@gmail.com>
2024-10-03 16:25:53 +0900

    React Ex1 complete - State

--- grounds/react/ra01/src/ExApp.tsx
+++ grounds/react/ra01/src/ExApp.tsx
@@ -1,7 +1,23 @@
 import { StateComponent1 } from "./examples/pages/ex01/StateComponent1";
 
+import styled from '@emotion/styled';
+import { StateComponent2 } from "./examples/pages/ex01/StateComponent2";
+import { PropsAndState } from "./examples/pages/ex01/PropsAndState";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
 function ExApp() {
-    return <StateComponent1></StateComponent1>;
+    return (
+        <Container>
+            <StateComponent1></StateComponent1>
+            <hr />
+            <StateComponent2></StateComponent2>
+            <hr />
+            <PropsAndState></PropsAndState>
+        </Container>
+    );
 }
 
 export default ExApp;
