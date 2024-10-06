c3f805f7f58f966fe436e83fd6823f48818b5506
Deckard <prodigy9090@gmail.com>
2024-10-06 23:17:49 +0900

    React Example : useEffect
    but, do not complete.
    
    What is this message?
    Argument of type '{}' is not assignable to parameter of type 'SetStateAction<readonly Post[]>'.ts(2345

--- grounds/react/ra01/src/ExLayApp.tsx
+++ grounds/react/ra01/src/ExLayApp.tsx
@@ -6,6 +6,7 @@ import styled from '@emotion/styled';
 import { Home } from './examples/pages/Home';
 import { About } from './examples/pages/About';
 import { Content2 } from './examples/pages/ex03/Contents/Content2';
+import { BlogPosts } from './examples/pages/BlogPosts';
 
 const NotFound = styled.div`
   text-align: center;
@@ -19,6 +20,7 @@ function ExLayApp() {
       <Route path="/about" element={<About />} />
       <Route path="/content1" element={<Content1 />} />
       <Route path="/content2" element={<Content2 />} />
+      <Route path="/mock/blog-posts" element={<BlogPosts />} />
       <Route
        path={'*'}
        element={
